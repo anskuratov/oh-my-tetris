1e732e3afc6a44fd7da5766e6b9e017fae8e53b1
Andrey Skuratov <>
2019-03-19 10:31:01 +0300

    added animations loader and scene loader

---
+++ Oh my tetris!/Assets/Scene_setup_main/AnimationsLoaderBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class AnimationsLoaderBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _loadingCanvasTransform;
+
+    [SerializeField]
+    private GameObject _loadingScreenPrefab;
+
+    [SerializeField]
+    private GameObject _successfulNotificationPrefab;
+
+    [SerializeField]
+    private GameObject _failedNotificationPrefab;
+
+    [SerializeField]
+    private int _notificationLifetimeDurationInSeconds = 3;
+
+    private GameObject _loadingScreenObject;
+
+    public void StartLoading() =>
+        _loadingScreenObject = Instantiate(_loadingScreenPrefab, transform);
+
+    public void StopLoading() =>
+        Destroy(_loadingScreenObject);
+
+    public async Task ShowSuccessScreenAsync()
+        => await ShowInfoScreenAsync(_successfulNotificationPrefab);
+
+    public async Task ShowFailedScreenAsync()
+        => await ShowInfoScreenAsync(_failedNotificationPrefab);
+
+    private async Task ShowInfoScreenAsync(GameObject screenPrefab)
+    {
+        var infoScreen = Instantiate(screenPrefab, transform);
+
+        await Task.Delay(TimeSpan.FromSeconds(_notificationLifetimeDurationInSeconds));
+
+        Destroy(infoScreen);
+    }
+}
---
+++ Oh my tetris!/Assets/Scene_setup_main/SceneLoaderBehaviour.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderBehaviour : MonoBehaviour
+{
+    private const float millisecondsToWait = 1000f;
+
+    [SerializeField]
+    private AnimationsLoaderBehaviour _animationsLoader;
+
+    private List<GameScene> _loadedScenes;
+
+    private void Awake()
+    {
+        _loadedScenes = new List<GameScene>();
+    }
+
+    public void LoadScene(GameScene sceneToLoad, bool unloadOtherScenes = false) =>
+        LoadSceneAsync(sceneToLoad, unloadOtherScenes);
+
+    private async Task LoadSceneAsync(GameScene sceneToLoad, bool unloadOtherScenes)
+    {
+        _animationsLoader.StartLoading();
+
+        var loadingTask = LoadSceneAsync(sceneToLoad);
+        while (loadingTask.IsCompleted == false)
+            await Task.Delay(TimeSpan.FromMilliseconds(millisecondsToWait));
+
+        if (unloadOtherScenes && _loadedScenes.Count != 0)
+        {
+            loadingTask = UnloadScenesAsync();
+            while (loadingTask.IsCompleted == false)
+                await Task.Delay(TimeSpan.FromMilliseconds(millisecondsToWait));
+        }
+
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex((int)sceneToLoad));
+
+        _loadedScenes.Add(sceneToLoad);
+
+        _animationsLoader.StopLoading();
+    }
+
+    private async Task UnloadScenesAsync()
+    {
+        foreach (var scene in _loadedScenes)
+            await UnloadSceneAsync(scene);
+
+        _loadedScenes.Clear();
+    }
+
+    private async Task LoadSceneAsync(GameScene sceneToLoad)
+    {
+        var loadingOperation = SceneManager.LoadSceneAsync((int)sceneToLoad, LoadSceneMode.Additive);
+
+        while (loadingOperation.isDone == false)
+            await Task.Delay(TimeSpan.FromMilliseconds(millisecondsToWait));
+    }
+
+    private async Task UnloadSceneAsync(GameScene sceneToUnload)
+    {
+        var unloadingOperation = SceneManager.UnloadSceneAsync((int)sceneToUnload);
+
+        while (unloadingOperation.isDone == false)
+            await Task.Delay(TimeSpan.FromMilliseconds(millisecondsToWait));
+    }
+}
+
+public enum GameScene
+{
+    level_main_menu,
+    level_game
+}
