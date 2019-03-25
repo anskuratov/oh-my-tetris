f15bd05d1d9fcd440accfa5e2fa5e7cd5c1e2d11
Andrey Skuratov <>
2019-03-25 22:00:52 +0300

    added game controller script and setup level_game scene

---
+++ Oh my tetris!/Assets/Scene_level_game/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Gameplay
+{
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _playingFieldParent;
+
+        [SerializeField]
+        private GameObject _playingFieldPrefab;
+
+        [SerializeField]
+        private GameObject _cellPrefab;
+
+        private PlayingFieldController _playingFieldController;
+        private GameModel _gameModel;
+
+        private PlayingFieldGenerator _playingFieldGenerator;
+
+        [Inject]
+        private void ZenjectInit(PlayingFieldGenerator playingFieldGenerator)
+        {
+            _playingFieldGenerator = playingFieldGenerator;
+        }
+
+        private void Awake()
+        {
+            _gameModel = new GameModel();
+        }
+
+        private void Start()
+        {
+            GenerateGameObjects();
+
+            StartCoroutine(GameTickCoroutine());
+        }
+
+        private IEnumerator GameTickCoroutine()
+        {
+            yield return new WaitForSeconds(_gameModel.SecondsBetweenTicks);
+        }
+
+        private void GenerateGameObjects()
+        {
+            var playingField = _playingFieldGenerator.Generate(
+                _gameModel.PlayingFieldWidth,
+                _gameModel.PlayingFieldHeight,
+                _playingFieldPrefab,
+                _cellPrefab);
+        }
+    }
+}
---
+++ Oh my tetris!/Assets/Scene_level_game/GameModel.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Assets.Gameplay
+{
+    public class GameModel
+    {
+        private const float _tickRateLevelDecrementor = 0.05f;
+        private const int _scoreIncrementor = 20;
+
+        public float SecondsBetweenTicks { get; private set; }
+        public int PlayingFieldWidth { get; private set; }
+        public int PlayingFieldHeight { get; private set; }
+        public int FigureSize { get; private set; }
+
+        private int _score;
+        public int Score
+        {
+            get
+            {
+                return _score;
+            }
+            private set
+            {
+                _score = value;
+
+                if (_score % 100 == 0)
+                    DecrementGameTickTime();
+            }
+        }
+
+        public GameModel()
+        {
+            SecondsBetweenTicks = 1f;
+            Score = 0;
+
+            PlayingFieldWidth = PlayerPrefs.GetInt("GameFieldWidth");
+            PlayingFieldHeight = PlayerPrefs.GetInt("GameFieldHeight");
+            FigureSize = PlayerPrefs.GetInt("FigureCellsNumber");
+        }
+
+        private void DecrementGameTickTime()
+            => SecondsBetweenTicks -= _tickRateLevelDecrementor;
+
+        public void IncrementScore()
+            => Score += _scoreIncrementor;
+    }
+}
